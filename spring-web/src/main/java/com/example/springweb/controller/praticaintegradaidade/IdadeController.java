43c1a07d4431856c9e12bb3d121eee1032b835fb
lucaspereirasilva1 <lucas.lpereira@mercadolivre.com>
2021-09-03 09:22:45 -0300

    alterado lógica do exercício patrica integrada idade

--- spring-web/src/main/java/com/example/springweb/controller/praticaintegradaidade/IdadeController.java
+++ spring-web/src/main/java/com/example/springweb/controller/praticaintegradaidade/IdadeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
 
 @RestController
@@ -14,9 +15,17 @@ public class IdadeController {
 
     @GetMapping("idade/{dia}/{mes}/{ano}")
     public int idadeGet(@PathVariable Integer dia, @PathVariable Integer mes, @PathVariable Integer ano) throws ParseException {
-        LocalDate localDate = LocalDate.now();
-        int anoAtual = localDate.getYear();
-        return anoAtual - ano;
+        String dataRecebida = dia.toString() + "/" + mes.toString() + "/" + ano.toString();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date dataConvertida = simpleDateFormat.parse(dataRecebida);
+        Calendar data = Calendar.getInstance();
+        Calendar dataAtual= Calendar.getInstance();
+        data.setTime(dataConvertida);
+        data.set(Calendar.YEAR, dataAtual.get(Calendar.YEAR));
+        int idade = data.after(dataAtual) ? -1 : 0;
+        data.setTime(dataConvertida);
+        idade += dataAtual.get(Calendar.YEAR) - data.get(Calendar.YEAR);
+        return idade;
     }
 
 }
